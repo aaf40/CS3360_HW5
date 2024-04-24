6eda784c1e94546c463162b3ed26f7c2422f91a7
aaf40 <aaf40@txstate.edu>
2024-04-24 15:05:31 -0500

    format

--- process.py
+++ process.py
@@ -2,15 +2,15 @@
 import itertools
 
 class Process:
-    _pid_generator = itertools.count(1)  # PID generator
+    _pid_generator = itertools.count(1)
 
     def __init__(self, arrival_time, service_time):
         self.arrival_time = arrival_time
         self.service_time = service_time
         self.pid = next(self._pid_generator)
-        self.wait_time = 0  # Initialize wait time at creation
+        self.wait_time = 0
 
     def update_wait_time(self, current_time):
-        self.wait_time += current_time - self.arrival_time  # Example logic
+        self.wait_time += current_time - self.arrival_time
 
 
